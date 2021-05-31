bfdd7f9e866f9c66fc0f1a6ca6d0ccb5b4024fc8
unknown <utsabmukherjee611@gmail.com>
2021-05-31 20:29:12 +0530

    flask last

--- sum.py
+++ sum.py
@@ -1,8 +1,29 @@
+from flask import Flask,jsonify,request 
+app = Flask(__name__)
 
-def sum(i):
-    if i==1:
-        return i
-    else:
-        return sum(i-1)+i
+@app.route('/',methods = ['GET'])
+def toThePower():
+    number = request.args['number']
+    print(type(number))
+    square = (int(number)**2)
+    cube = (int(number)**3)
+    
 
-print(sum(10))        
+    #print(number)
+    return jsonify({"data":[{"square":square,"cube":cube}]})
+
+@app.route('/sum',methods = ['POST','GET'])
+def getSum():
+    number = request.get_json()
+    first_number=number['first_number']
+    second_number=number['second_number']
+    print(type(first_number))
+    sum = first_number + second_number
+    print(number)
+    return jsonify({"data":[{"sum":sum}]})
+    
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True) 
